e910a6660526c1eb3d1fce539f79e85c9860948c
Ryan Lieu <github-benzBrake@woai.ru>
2022-03-15 11:39:27 +0800

    feat: 新增 git 缩写

---
+++ aliases/git.aliases.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+if [ -n "$(command -v git)" ]; then
+    alias og='less $EZ_HOME/aliases/git.aliases.sh'
+    alias ga='git add'
+    alias gc='git commit -v'
+    alias gd='git diff'
+    alias gst='git status'
+    alias gcl='git gcl'
+    alias gch='git checkout'
+    alias gcm='git checkout master'
+    alias gps='git push'
+    alias gpu='git pull'
+fi

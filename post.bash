942e1ba9e10c034c545071b19e4c91b6f4478693
Ryan Lieu <github-benzBrake@woai.ru>
2022-03-15 11:45:12 +0800

    fix: dircolors not found

--- post.bash
+++ post.bash
@@ -2,7 +2,7 @@
 # @Description: 最后载入的脚本
 ### 
 # remove background colors from dircolors
-if [ ! -n "$(command -v dircolors)" ]; then
+if [ -n "$(command -v dircolors)" ]; then
     eval "$(dircolors -p | \
         sed 's/ 4[0-9];/ 01;/; s/;4[0-9];/;01;/g; s/;4[0-9] /;01 /' | \
         dircolors /dev/stdin)"

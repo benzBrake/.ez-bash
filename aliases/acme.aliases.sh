5d7b93005a52918482452c6eeda3849042100574
Ryan Lieu <github-benzBrake@woai.ru>
2022-03-15 09:11:31 +0800

    feat: add wsl & acme aliases

---
+++ aliases/acme.aliases.sh
@@ -0,0 +1,16 @@
+function instcert() {
+    if [ ! -f ~/.acme.sh ]; then
+	echo Please Install acme.sh first!
+        return
+    fi
+    if [ $# != 1 ] ; then
+        echo Usage: instcert [domain]
+    else
+        DOMAIN="$1"
+        . "/root/.acme.sh/acme.sh.env"
+        /root/.acme.sh/acme.sh --install-cert -d "$DOMAIN" \
+          --key-file "/data/web/webcert/${DOMAIN}.key" \
+          --fullchain-file "/data/web/webcert/${DOMAIN}.crt" \
+          --reloadcmd "docker-compose -f /data/docker-compose.yml restart nginx"
+   fi
+}

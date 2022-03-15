f45428bd7a84a224546554a338221166c4b35239
Ryan Lieu <github-benzBrake@woai.ru>
2022-03-15 11:24:20 +0800

    fix: 防止没有 dircolors 命令

--- post.bash
+++ post.bash
@@ -2,6 +2,8 @@
 # @Description: 最后载入的脚本
 ### 
 # remove background colors from dircolors
-eval "$(dircolors -p | \
-    sed 's/ 4[0-9];/ 01;/; s/;4[0-9];/;01;/g; s/;4[0-9] /;01 /' | \
-    dircolors /dev/stdin)"
+if [ ! -n "$(command -v dircolors)" ]; then
+    eval "$(dircolors -p | \
+        sed 's/ 4[0-9];/ 01;/; s/;4[0-9];/;01;/g; s/;4[0-9] /;01 /' | \
+        dircolors /dev/stdin)"
+fi

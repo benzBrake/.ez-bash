f934e841f5c617b4aacd88597321f2a24a3dbeea
Ryan Lieu <github-benzBrake@woai.ru>
2022-03-20 16:40:30 +0800

    feat: add 3 git aliases

--- aliases/git.aliases.sh
+++ aliases/git.aliases.sh
@@ -2,6 +2,7 @@
 if [ -n "$(command -v git)" ]; then
     alias og='less $EZ_HOME/aliases/git.aliases.sh'
     alias ga='git add'
+    alias gb='git branch'
     alias gc='git commit -v'
     alias gd='git diff'
     alias gst='git status'
@@ -9,5 +10,9 @@ if [ -n "$(command -v git)" ]; then
     alias gch='git checkout'
     alias gcm='git checkout master'
     alias gps='git push'
+    alias gpt='git push origin'
     alias gpu='git pull'
+    alias gm='git merge'
+    alias gt='git tag'
+    alias grs='git reset'
 fi

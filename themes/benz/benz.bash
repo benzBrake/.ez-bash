98c377ea25aee996488424667b510e80500c609b
Ryan Lieu <github-benzBrake@woai.ru>
2026-07-12 17:04:53 +0800

    style(benz): 调整折叠路径显示颜色为深绿色
    - 降低 collapsed_pwd 路径颜色亮度，提升终端可读性
    
    - 同时调整 root 用户与普通用户的提示符配色

--- themes/benz/benz.bash
+++ themes/benz/benz.bash
@@ -40,9 +40,9 @@ function _fish_collapsed_pwd() {
 # custom prompt
 if [ -n "$BASH_VERSION" ]; then
     if [ "$UID" -eq 0 ]; then
-        export PS1='\[\e[38;5;135m\]\u\[\e[0m\]@\[\e[38;5;166m\]\h\[\e[0m\] \[\e[38;5;118m\]$(_fish_collapsed_pwd)\[\e[0m\]# '
+        export PS1='\[\e[38;5;135m\]\u\[\e[0m\]@\[\e[38;5;166m\]\h\[\e[0m\] \[\e[38;5;30m\]$(_fish_collapsed_pwd)\[\e[0m\]# '
     else
-        export PS1='\[\e[38;5;135m\]\u\[\e[0m\]@\[\e[38;5;166m\]\h\[\e[0m\] \[\e[38;5;118m\]$(_fish_collapsed_pwd)\[\e[0m\]> '
+        export PS1='\[\e[38;5;135m\]\u\[\e[0m\]@\[\e[38;5;166m\]\h\[\e[0m\] \[\e[38;5;30m\]$(_fish_collapsed_pwd)\[\e[0m\]> '
     fi
 else
     if [ $UID -eq 0 ]; then
